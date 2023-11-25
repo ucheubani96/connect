f288521bcb93823ed9606d538495a2e3687a14a1
ucheubani96 <mac@Joey-Macbook.local>
2023-11-25 21:14:25 +0000

    Fixed repository queries

--- src/main/java/com/example/connect/connections/ConnectionsRepo.java
+++ src/main/java/com/example/connect/connections/ConnectionsRepo.java
@@ -7,6 +7,7 @@ import org.springframework.lang.NonNull;
 import java.util.List;
 
 public interface ConnectionsRepo extends JpaRepository<Connections, Long> {
+    @Query("select c from Connections c where c.first_id = ?1 or c.second_id = ?2")
     List<Connections> findByFirst_idOrSecond_id(@NonNull Long first_id, @NonNull Long second_id);
 
 }
--- src/main/java/com/example/connect/userVerification/UserVerificationRepo.java
+++ src/main/java/com/example/connect/userVerification/UserVerificationRepo.java
@@ -1,10 +1,14 @@
 package com.example.connect.userVerification;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserVerificationRepo extends JpaRepository<UserVerification, Long> {
+    @Query("select uv from UserVerification uv where uv.user_id = ?1 ")
     Optional<UserVerification> findByUser_id(Long userId);
+
+    @Query("select uv from UserVerification uv where uv.token = ?1 ")
     Optional<UserVerification> findByToken(String token);
 }
